32c6a60b600c11e18ecbc4cef71bb654411072f4
jtvandyk <justin@edbuild.org>
2015-05-20 10:22:16 -0400

    State table create script
    Generic script for table creation

---
+++ sql/tablecreate.sql
@@ -0,0 +1,19 @@
+SELECT "sei"."master"."GID",
+"sei"."master"."dStateFP",
+"sei"."master"."dNCESID",
+"sei"."master"."dName",
+"sei"."master"."dLoGrade",
+"sei"."master"."dHiGrade",
+"sei"."master"."dStatePost",
+"sei"."master"."dEstPop",
+"sei"."master"."dEstStud",
+"sei"."master"."dEstPov",
+"sei"."master"."dEstPovRate",
+"sei"."master"."year",
+"sei"."master"."geom"
+--Type in the two character state code for the creation of the relevant table
+INTO "sei"."<StateFP>"
+FROM "sei"."master"
+--Copy the state character code used in above here
+WHERE "sei"."master"."dStateFP" = '--<dStateFP>'
+ORDERED BY "sei"."master"."dName"
