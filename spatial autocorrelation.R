5b54452a17be1ad2cef8e7f591c0f9cbebdea7d7
Justin van Dyk <justin@ip-172-31-47-33.ec2.internal>
2015-05-20 14:35:06 +0000

    Creation of shells for master scripts.

---
+++ spatial autocorrelation.R
@@ -0,0 +1,13 @@
+require(RPostgreSQL)
+
+## loads the PostgreSQL driver
+drv <- dbDriver("PostgreSQL")
+
+## Open a connection
+con <- dbConnect(drv, 
+                 dbname="eb1",
+                 host = "edbuild1.c85mgedxi7oy.us-east-1.rds.amazonaws.com",
+                 port = 5432,
+                 user = "ebadmin",
+                 password = "Edbuild2014"
+)
