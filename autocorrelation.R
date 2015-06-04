13bae00c03d946dbc913273670d4e9af67f506c7
Justin van Dyk <justin@ip-172-31-47-33.ec2.internal>
2015-06-04 19:56:02 +0000

    spacing & naming fix

--- autocorrelation.R
+++ autocorrelation.R
@@ -12,10 +12,9 @@ NJ.coords <- coordinates(NJ.sp)
 par(mar=rep(0,4))
 plot(NJ.lw,coords=NJ.coords,pch=19, cex=0.1, col="gray")
 
-## Global Autocorrelation Tests: Moran's I
+# Global Autocorrelation Tests: Moran's I
 moran.test(NJ$EstPovRate, listw=NJ.lw, zero.policy=T)
 
 
-## Global Autocorrelation Tests: Geary's C
-
+# Global Autocorrelation Tests: Geary's C
 geary.test(NJ$EstPovRate, listw=NJ.lw, zero.policy=T)
