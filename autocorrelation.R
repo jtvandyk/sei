af32218a87d766c05e1e9ad39ed5f2f5d30af2a7
Justin van Dyk <justin@ip-172-31-47-33.ec2.internal>
2015-06-04 19:59:19 +0000

    local morans i test

--- autocorrelation.R
+++ autocorrelation.R
@@ -23,3 +23,12 @@ geary.test(NJ$EstPovRate, listw=NJ.lw, zero.policy=T)
 par(mar=c(4,4,1.5,0.5))
 moran.plot(NJ$EstPovRate, listw=NJ.lw, zero.policy=T, xlim=c(0,100),ylim=c(0,100), pch=16, col="black",cex=.5, quiet=F, labels=as.character(data$NAME),xlab="Percent for Bush", ylab="Percent for Bush (Spatial Lag)", main="Moran Scatterplot")
 
+## Local Autocorrelation: Local Moran's I (normality assumption)
+lm1 <- localmoran(NJ$EstPovRate, listw=NJ.lw, zero.policy=T)
+
+NJ$lm1 <- abs(lm1[,4]) ## Extract z-scores
+
+lm.palette <- colorRampPalette(c("white","orange", "red"), space = "rgb")
+spplot(NJ, zcol="lm1", col.regions=lm.palette(20), main="Local Moran's I (|z| scores)", pretty=T)
+
+
