3ab9b3a93a08232ad4411ebcde2890cec9791830
Justin van Dyk <justin@ip-172-31-47-33.ec2.internal>
2015-06-04 19:57:20 +0000

    add moran scatterplot

--- autocorrelation.R
+++ autocorrelation.R
@@ -18,3 +18,8 @@ moran.test(NJ$EstPovRate, listw=NJ.lw, zero.policy=T)
 
 # Global Autocorrelation Tests: Geary's C
 geary.test(NJ$EstPovRate, listw=NJ.lw, zero.policy=T)
+
+# Moran Scatterplot
+par(mar=c(4,4,1.5,0.5))
+moran.plot(NJ$EstPovRate, listw=NJ.lw, zero.policy=T, xlim=c(0,100),ylim=c(0,100), pch=16, col="black",cex=.5, quiet=F, labels=as.character(data$NAME),xlab="Percent for Bush", ylab="Percent for Bush (Spatial Lag)", main="Moran Scatterplot")
+
